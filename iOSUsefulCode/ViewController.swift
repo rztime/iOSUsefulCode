b34389ac9f71cb3c5a2f46a3f143bc0dc9cdb340
rztime <rztime@vip.qq.com>
2020-03-24 12:37:28 +0800

    Initial Commit

---
+++ iOSUsefulCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSUsefulCode
+//
+//  Created by ruozui on 2020/3/24.
+//  Copyright © 2020 rztime. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
